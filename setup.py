d5c3fc6d157dac3bbc831f566218259d4ea5d60c
Fredrick W. Warren <fredw@northriverboats.com>
2021-03-09 10:48:44 -0800

    added missing quote mark in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="an email wrapper",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/northriverboats/emailer,
+    url="https://github.com/northriverboats/emailer",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
